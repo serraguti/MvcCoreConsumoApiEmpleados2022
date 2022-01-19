621d43c693897b1700970f4ae2b5aa62818d1719
serraguti <46522403+serraguti@users.noreply.github.com>
2022-01-19 20:31:38 +0100

    Add project files.

---
+++ MvcCoreConsumoApiEmpleados2022/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcCoreConsumoApiEmpleados2022.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MvcCoreConsumoApiEmpleados2022/Services/ServiceApiEmpleados.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using LibrariesEmpleado;
+
+namespace MvcCoreConsumoApiEmpleados2022.Services
+{
+    public class ServiceApiEmpleados
+    {
+        private string Url;
+        private MediaTypeWithQualityHeaderValue header;
+
+        public ServiceApiEmpleados(String url)
+        {
+            this.Url = url;
+            this.header =
+                new MediaTypeWithQualityHeaderValue("application/json");
+        }
+
+        public async Task<List<Empleado>> GetEmpleadosAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "/api/empleados";
+                client.BaseAddress = new Uri(this.Url);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.header);
+                HttpResponseMessage response =
+                    await client.GetAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    List<Empleado> empleados =
+                        await response.Content.ReadAsAsync<List<Empleado>>();
+                    return empleados;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
